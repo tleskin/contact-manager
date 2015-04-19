7fa2b9bd8d12bb9da1da154c97ffaba92784615b
Thomas Leskin <tleskin@gmail.com>
2015-04-19 09:06:46 -0600

    Added EmailAddress model and created relationship to people

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150419011112) do
+ActiveRecord::Schema.define(version: 20150419150300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "email_addresses", force: :cascade do |t|
+    t.string   "address"
+    t.integer  "person_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "people", force: :cascade do |t|
     t.string   "first_name"
