32c7892c34bc7ca18e8f4a707ca6a590a304b6fc
Thomas Leskin <tleskin@gmail.com>
2015-04-18 20:39:58 -0600

    User can add a phone number

--- Gemfile
+++ Gemfile
@@ -43,4 +43,6 @@ group :development, :test do
   gem 'spring'
 
   gem 'rspec-rails'
+
+  gem 'capybara'
 end
--- spec/controllers/phone_numbers_controller_spec.rb
+++ spec/controllers/phone_numbers_controller_spec.rb
@@ -69,6 +69,10 @@ RSpec.describe PhoneNumbersController, type: :controller do
 
   describe "POST #create" do
     context "with valid params" do
+
+      let(:alice) { Person.create(first_name: 'Alice', last_name: 'Smith') }
+      let(:valid_attributes) { {number: '555-1234', person_id: alice.id} }
+      
       it "creates a new PhoneNumber" do
         expect {
           post :create, {:phone_number => valid_attributes}, valid_session
@@ -82,8 +86,10 @@ RSpec.describe PhoneNumbersController, type: :controller do
       end
 
       it "redirects to the created phone_number" do
-        post :create, {:phone_number => valid_attributes}, valid_session
-        expect(response).to redirect_to(PhoneNumber.last)
+        alice = Person.create(first_name: 'Alice', last_name: 'Smith')
+        valid_attributes = {number: '555-8888', person_id: alice.id}
+        post :create, {phone_number: valid_attributes}, valid_session
+        expect(response).to redirect_to(alice)
       end
     end
 
